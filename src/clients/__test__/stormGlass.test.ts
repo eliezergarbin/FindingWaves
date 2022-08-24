41759d7fc164918d0804c0eefa259142a9d5460d
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-08-24 20:02:48 -0300

    Criando o API client para consumir a API externa

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,19 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    axios.get = jest.fn().mockResolvedValue({ stormglassWeatherPointFixture });
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual({stormglassNormalizedResponseFixture});
+  });
+});
---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,15 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPISource = 'noaa';
+  constructor(protected request: AxiosStatic) {}
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    return this.request.get(`
+    https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}
+    `);
+  }
+}
