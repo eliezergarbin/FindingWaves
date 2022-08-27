18f40acbea1f9d266f28ed808d4194faaba8d750
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-08-27 12:01:14 -0300

    Request client com Typescript generics

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,19 +1,25 @@
 import { StormGlass } from '@src/clients/stormGlass';
-import axios from 'axios';
+import * as HTTPUtil from '@src/util/request';
 import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
-jest.mock('axios');
+jest.mock('@src/util/request');
 
 describe('StormGlass client', () => {
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  const MockedRequestClass = HTTPUtil.Request as jest.Mocked<
+    typeof HTTPUtil.Request
+  >;
+
+  const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>;
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    mockedAxios.get.mockResolvedValue({ data: stormglassWeatherPointFixture });
+    mockedRequest.get.mockResolvedValue({
+      data: stormglassWeatherPointFixture,
+    } as HTTPUtil.Response);
 
-    const stormGlass = new StormGlass(mockedAxios);
+    const stormGlass = new StormGlass(mockedRequest);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormglassNormalizedResponseFixture);
   });
@@ -31,9 +37,11 @@ describe('StormGlass client', () => {
         },
       ],
     };
-    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+    mockedRequest.get.mockResolvedValue({
+      data: incompleteResponse,
+    } as HTTPUtil.Response);
 
-    const stormGlass = new StormGlass(mockedAxios);
+    const stormGlass = new StormGlass(mockedRequest);
     const response = await stormGlass.fetchPoints(lat, lng);
 
     expect(response).toEqual([]);
@@ -43,12 +51,12 @@ describe('StormGlass client', () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    mockedAxios.get.mockRejectedValue({ message: 'Network Error' });
+    mockedRequest.get.mockRejectedValue('Network Error');
 
-    const stormGlass = new StormGlass(mockedAxios);
+    const stormGlass = new StormGlass(mockedRequest);
 
     await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
-      'Unexpected error when trying to communicate to StormGlass: Network Error'
+      'Unexpected error when trying to communicate to StormGlass: "Network Error"'
     );
   });
 
@@ -62,14 +70,21 @@ describe('StormGlass client', () => {
       }
     }
 
-    mockedAxios.get.mockRejectedValue(
+    mockedRequest.get.mockRejectedValue(
       new FakeAxiosError({
         status: 429,
         data: { errors: ['Rate Limit reached'] },
       })
     );
 
-    const stormGlass = new StormGlass(mockedAxios);
+    MockedRequestClass.isRequestError.mockReturnValue(true);
+
+    MockedRequestClass.extractErrorData.mockReturnValue({
+      status: 429,
+      data: { errors: ['Rate Limit reached'] },
+    });
+
+    const stormGlass = new StormGlass(mockedRequest);
 
     await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
       'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
---
+++ src/util/request.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RequestConfig extends AxiosRequestConfig {}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-interface
+export interface Response<T = any> extends AxiosResponse<T> {}
+
+export class Request {
+  constructor(private request = axios) {}
+  public get<T>(url: string, config: RequestConfig = {}): Promise<Response<T>> {
+    return this.request.get<T, Response<T>>(url, config);
+  }
+  public static isRequestError(error: Error): boolean {
+    return !!(
+      (error as AxiosError).response && (error as AxiosError).response?.status
+    );
+  }
+  public static extractErrorData(
+    error: unknown
+  ): Pick<AxiosResponse, 'data' | 'status'> {
+    const axiosError = error as AxiosError;
+    if (axiosError.response && axiosError.response.status) {
+      return {
+        data: axiosError.response.data,
+        status: axiosError.response.status,
+      };
+    }
+    throw Error(`The error ${error} is not a Request Error`);
+  }
+}
