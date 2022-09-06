58b4f11f6e1f7e5b78741ea1bbb466ecb8541088
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-09-06 18:40:07 -0300

    Mongoose hooks, validation e Type narrowing

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -5,6 +5,7 @@ describe('Users functional tests', () => {
     beforeEach(async () => {
       await User.deleteMany({});
     });
+
     it('should sucessfully create a new user', async () => {
       const newUser = {
         name: 'John Doe',
@@ -15,6 +16,36 @@ describe('Users functional tests', () => {
       const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+
+    it('Should return 422 when there is a validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: 'password',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
+
+    it('Should return 409 when the email already exists', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exists in the database.',
+      });
     });
   });
 });
