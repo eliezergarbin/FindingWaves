8b597699e35013a0e69e3f7806e4a9c05a2fc7db
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-09-09 17:37:56 -0300

    atualizando jest

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
+// eslint-disable-next-line no-undef
 module.exports = {...rootConfig, ...{
   rootDir: root,
   displayName: "end2end-tests",
